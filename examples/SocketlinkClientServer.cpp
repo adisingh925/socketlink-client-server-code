dc2cc7772e7210b3311fe59bd28bc44f59e49821
Aditya Singh <adisingh925@gmail.com>
2025-03-07 00:42:36 +0530

    na

--- examples/SocketlinkClientServer.cpp
+++ examples/SocketlinkClientServer.cpp
@@ -2022,39 +2022,39 @@ void openConnection(uWS::WebSocket<true, true, PerSocketData>* ws, worker_t* wor
             }
         }    
 
-        {
-            /** Acquire an accessor for the outer map */ 
-            tbb::concurrent_hash_map<std::string, tbb::concurrent_hash_map<std::string, uint8_t>>::accessor uid_to_rid_outer_accessor;
+        // {
+        //     /** Acquire an accessor for the outer map */ 
+        //     tbb::concurrent_hash_map<std::string, tbb::concurrent_hash_map<std::string, uint8_t>>::accessor uid_to_rid_outer_accessor;
         
-            /** Check if UID exists */ 
-            if (uidToRoomMapping.find(uid_to_rid_outer_accessor, uid)) {
-                auto& innerMap = uid_to_rid_outer_accessor->second;
+        //     /** Check if UID exists */ 
+        //     if (uidToRoomMapping.find(uid_to_rid_outer_accessor, uid)) {
+        //         auto& innerMap = uid_to_rid_outer_accessor->second;
                 
-                /** Acquire an accessor for the inner map */ 
-                tbb::concurrent_hash_map<std::string, uint8_t>::accessor uid_to_rid_inner_accessor;
+        //         /** Acquire an accessor for the inner map */ 
+        //         tbb::concurrent_hash_map<std::string, uint8_t>::accessor uid_to_rid_inner_accessor;
 
-                if (innerMap.insert(uid_to_rid_inner_accessor, rid)) {
-                    /** Only set roomType if insertion was successful */ 
-                    uid_to_rid_inner_accessor->second = roomType;
-                }
-            } else {
-                /** Create and insert a new inner map directly */ 
-                tbb::concurrent_hash_map<std::string, uint8_t> newInnerMap;
-                newInnerMap.emplace(rid, roomType);
+        //         if (innerMap.insert(uid_to_rid_inner_accessor, rid)) {
+        //             /** Only set roomType if insertion was successful */ 
+        //             uid_to_rid_inner_accessor->second = roomType;
+        //         }
+        //     } else {
+        //         /** Create and insert a new inner map directly */ 
+        //         tbb::concurrent_hash_map<std::string, uint8_t> newInnerMap;
+        //         newInnerMap.emplace(rid, roomType);
         
-                if (uidToRoomMapping.insert(uid_to_rid_outer_accessor, uid)) {
-                    uid_to_rid_outer_accessor->second = std::move(newInnerMap);
-                }
+        //         if (uidToRoomMapping.insert(uid_to_rid_outer_accessor, uid)) {
+        //             uid_to_rid_outer_accessor->second = std::move(newInnerMap);
+        //         }
 
-                /** Check if the uid map has the value true, make it false else ignore */
-                tbb::concurrent_hash_map<std::string, bool>::accessor uid_outer_accessor;
-                if (::uid.find(uid_outer_accessor, uid)) {
-                    if (uid_outer_accessor->second) { 
-                        uid_outer_accessor->second = false;
-                    }
-                }
-            }
-        }   
+        //         /** Check if the uid map has the value true, make it false else ignore */
+        //         tbb::concurrent_hash_map<std::string, bool>::accessor uid_outer_accessor;
+        //         if (::uid.find(uid_outer_accessor, uid)) {
+        //             if (uid_outer_accessor->second) { 
+        //                 uid_outer_accessor->second = false;
+        //             }
+        //         }
+        //     }
+        // }   
     
         /** Send a message to self */
         std::string selfMessage = "{\"data\":\"CONNECTED_TO_ROOM\", \"uid\":\"" + uid + "\", \"source\":\"server\", \"rid\":\"" + rid + "\"}";
