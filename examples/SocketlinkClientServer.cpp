cd1ceac29e8e7d3298f902a4bf24731a60166aee
Aditya Singh <adisingh925@gmail.com>
2025-03-26 19:25:51 +0530

    na

--- examples/SocketlinkClientServer.cpp
+++ examples/SocketlinkClientServer.cpp
@@ -846,7 +846,7 @@ void init_env() {
     }
 
     /** map size is 10 GB */
-    if (mdb_env_set_mapsize(env, UserData::getInstance().lmdbDatabaseSizeInBytes) != 0) { 
+    if (mdb_env_set_mapsize(env, 20ULL * 1024 * 1024 * 1024) != 0) { 
         std::cerr << "Failed to set map size.\n";
         exit(-1);
     }
