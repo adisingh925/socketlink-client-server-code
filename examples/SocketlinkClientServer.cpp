cd21da4f6954c1028d45bb1eb8f6a35bd72653e3
Aditya Singh <adisingh925@gmail.com>
2025-04-19 14:55:56 +0530

    na

--- examples/SocketlinkClientServer.cpp
+++ examples/SocketlinkClientServer.cpp
@@ -1718,64 +1718,64 @@ void openConnection(uWS::WebSocket<true, true, PerSocketData>* ws, worker_t* wor
             size = inner_set.size();
         }   
 
-        if(isMultiThread) {
-            log("going inside the multi-threaded block");
+        // if(isMultiThread) {
+        //     log("going inside the multi-threaded block");
 
-            /** Acquire an accessor for the outer map */ 
-            tbb::concurrent_hash_map<std::string, tbb::concurrent_hash_map<std::string, uint8_t>>::accessor uid_to_rid_outer_accessor;
+        //     /** Acquire an accessor for the outer map */ 
+        //     tbb::concurrent_hash_map<std::string, tbb::concurrent_hash_map<std::string, uint8_t>>::accessor uid_to_rid_outer_accessor;
         
-            /** Check if UID exists */ 
-            if (ThreadSafe::uidToRoomMapping.find(uid_to_rid_outer_accessor, uid)) {
-                log("uid exists");
+        //     /** Check if UID exists */ 
+        //     if (ThreadSafe::uidToRoomMapping.find(uid_to_rid_outer_accessor, uid)) {
+        //         log("uid exists");
 
-                auto& innerMap = uid_to_rid_outer_accessor->second;
+        //         auto& innerMap = uid_to_rid_outer_accessor->second;
                 
-                /** Acquire an accessor for the inner map */ 
-                tbb::concurrent_hash_map<std::string, uint8_t>::accessor uid_to_rid_inner_accessor;
+        //         /** Acquire an accessor for the inner map */ 
+        //         tbb::concurrent_hash_map<std::string, uint8_t>::accessor uid_to_rid_inner_accessor;
 
-                if (innerMap.insert(uid_to_rid_inner_accessor, rid)) {
-                    log("inserting the rid");
+        //         if (innerMap.insert(uid_to_rid_inner_accessor, rid)) {
+        //             log("inserting the rid");
 
-                    /** Only set roomType if insertion was successful */ 
-                    uid_to_rid_inner_accessor->second = roomType;
-                }
-            } else {
-                log("uid does not exist");
+        //             /** Only set roomType if insertion was successful */ 
+        //             uid_to_rid_inner_accessor->second = roomType;
+        //         }
+        //     } else {
+        //         log("uid does not exist");
 
-                /** Create and insert a new inner map directly */ 
-                tbb::concurrent_hash_map<std::string, uint8_t> newInnerMap;
-                newInnerMap.emplace(rid, roomType);
+        //         /** Create and insert a new inner map directly */ 
+        //         tbb::concurrent_hash_map<std::string, uint8_t> newInnerMap;
+        //         newInnerMap.emplace(rid, roomType);
         
-                if (ThreadSafe::uidToRoomMapping.insert(uid_to_rid_outer_accessor, uid)) {
-                    log("inserting the uid");
+        //         if (ThreadSafe::uidToRoomMapping.insert(uid_to_rid_outer_accessor, uid)) {
+        //             log("inserting the uid");
 
-                    uid_to_rid_outer_accessor->second = std::move(newInnerMap);
-                }
+        //             uid_to_rid_outer_accessor->second = std::move(newInnerMap);
+        //         }
 
-                /** Check if the uid map has the value true, make it false else ignore */
-                tbb::concurrent_hash_map<std::string, bool>::accessor uid_outer_accessor;
-                if (ThreadSafe::uid.find(uid_outer_accessor, uid)) {
-                    log("uid exists in uid map");
+        //         /** Check if the uid map has the value true, make it false else ignore */
+        //         tbb::concurrent_hash_map<std::string, bool>::accessor uid_outer_accessor;
+        //         if (ThreadSafe::uid.find(uid_outer_accessor, uid)) {
+        //             log("uid exists in uid map");
 
-                    if (uid_outer_accessor->second) { 
-                        log("setting the uid to false");
+        //             if (uid_outer_accessor->second) { 
+        //                 log("setting the uid to false");
 
-                        uid_outer_accessor->second = false;
-                    }
-                } else {
-                    log("uid does not exist in uid map");
-                }
-            }
-        } else {
-            /** Try inserting the UID into uidToRoomMapping */
-            auto result = SingleThreaded::uidToRoomMapping.try_emplace(uid);
-            result.first->second.emplace(rid, roomType); 
+        //                 uid_outer_accessor->second = false;
+        //             }
+        //         } else {
+        //             log("uid does not exist in uid map");
+        //         }
+        //     }
+        // } else {
+        //     /** Try inserting the UID into uidToRoomMapping */
+        //     auto result = SingleThreaded::uidToRoomMapping.try_emplace(uid);
+        //     result.first->second.emplace(rid, roomType); 
 
-            /** Check if UID exists in SingleThreaded::uid and update if needed */
-            if (auto it2 = SingleThreaded::uid.find(uid); it2 != SingleThreaded::uid.end() && it2->second) {
-                it2->second = false;
-            }
-        }   
+        //     /** Check if UID exists in SingleThreaded::uid and update if needed */
+        //     if (auto it2 = SingleThreaded::uid.find(uid); it2 != SingleThreaded::uid.end() && it2->second) {
+        //         it2->second = false;
+        //     }
+        // }   
 
         log("finished inserting the uid and rid");
     
