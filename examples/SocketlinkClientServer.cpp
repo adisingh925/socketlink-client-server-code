9b37c50d367bc84cbca5b51fa1107d41d0263963
Aditya Singh <adisingh925@gmail.com>
2025-05-01 01:39:57 +0530

    na

--- examples/SocketlinkClientServer.cpp
+++ examples/SocketlinkClientServer.cpp
@@ -2034,7 +2034,7 @@ void worker_t::work()
   /* Very simple WebSocket broadcasting echo server */
   app_->ws<PerSocketData>("/*", {
     /* Settings */
-    .compression = uWS::DISABLED,
+    .compression = uWS::CompressOptions(uWS::DEDICATED_COMPRESSOR | uWS::DEDICATED_DECOMPRESSOR),
     .maxPayloadLength = UserData::getInstance().msgSizeAllowedInBytes,
     .idleTimeout = UserData::getInstance().idleTimeoutInSeconds,
     .maxBackpressure = UserData::getInstance().maxBackpressureInBytes,
